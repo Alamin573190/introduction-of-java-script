3ef2a14ddf6f15aabb0a744e134c24e3d5f85fe4
Alamin <mdalaminhossain573190@gmail.com>
2024-01-27 11:14:56 +0600

    this is a ternary commit

---
+++ ternary.js
@@ -0,0 +1,11 @@
+// .......ternary operator.........
+const age = 18;
+
+// if(age >=18){
+//     console.log('you can vote');
+// }
+// else{
+//     console.log('you can not vote')
+// }
+
+ age >= 18 ? console.log("you can vote") : console.log("you can not vot")
