2813c9125b9d1724221f7f779e5b56630a632d9a
Alamin <mdalaminhossain573190@gmail.com>
2024-01-27 16:05:51 +0600

    taks one

---
+++ barger.js
@@ -0,0 +1,10 @@
+// .....taks one....
+
+const burgerPrice = 510 ;
+
+if(burgerPrice > 500){
+    console.log('got a free coke')
+}
+else{
+    console.log('buy a coke 30tk')
+}
--- ternary.js
+++ ternary.js
@@ -1,5 +1,5 @@
 // .......ternary operator.........
-const age = 18;
+// const age = 18;
 
 // if(age >=18){
 //     console.log('you can vote');
@@ -8,4 +8,22 @@ const age = 18;
 //     console.log('you can not vote')
 // }
 
- age >= 18 ? console.log("you can vote") : console.log("you can not vot")
+//  age >= 18 ? console.log("you can vote") : console.log("you can not vot") 
+
+// ..........one more examplr for ternary.........
+
+// let price = 500 ;
+// const isLeader = true ; 
+ 
+// if(isLeader == true){
+//     price = 0 ;
+
+// }
+// else{
+//     price = price + 100 ;
+// }
+// console.log(price) ;
+
+// //   .....ternary rop.....
+
+// price = isLeader === true ? 0 : price + 100 ;
