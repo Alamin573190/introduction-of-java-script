ea5d330940860226d2eb2ebe40aa2e84c927c717
Alamin <mdalaminhossain573190@gmail.com>
2024-01-28 12:49:07 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,27 @@
+const dayOfWeek = 5 ;
+
+switch(dayOfWeek){
+    case 1 :
+        console.log('saturday');
+        break;
+    case 2 :
+        console.log('sunday');
+        break;
+    case 3 :
+        console.log('monday');
+        break;
+    case 4 :
+        console.log('tuseday');
+        break;
+    case 5 :
+        console.log('wednesday');
+        break;
+    case 6 :
+        console.log('thuseday');
+        break;
+    case 7 :
+        console.log('friday');
+        break;
+   default:
+    console.log('input invalid data')
+}
